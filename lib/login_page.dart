9f6e6de072fdd2e43c8417826b9bc946b8198e61
Gio Ciudadano <gaciudadano@up.edu.ph>
2023-02-23 19:35:03 +0800

    Adds account creation system using Firebase

--- lib/login_page.dart
+++ lib/login_page.dart
@@ -2,6 +2,8 @@ import 'package:bullet/signup_page.dart';
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
--- lib/signup_page.dart
+++ lib/signup_page.dart
@@ -1,7 +1,10 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignupPage extends StatelessWidget {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  var _inputEmail = TextEditingController();
+  var _inputPassword = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -22,22 +25,21 @@ class SignupPage extends StatelessWidget {
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
                       child: TextFormField(
+                        controller: _inputEmail,
                         decoration: InputDecoration(
                         border: OutlineInputBorder(),
                         labelText: 'Email',
                         hintText: 'Enter your email',
                         ),
                         validator: (String? value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter a username';
-                          }
-                          return null;
+                          return _verifyEmail(value);
                         },
                       ),
                     ),
                     Padding(
                       padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 4),
                       child: TextFormField(
+                        controller: _inputPassword,
                         obscureText: true,
                         decoration: InputDecoration(
                           border: OutlineInputBorder(),
@@ -45,10 +47,7 @@ class SignupPage extends StatelessWidget {
                           hintText: 'Enter your password',
                         ),
                         validator: (String? value) {
-                          if (value == null || value.isEmpty) {
-                            return 'Please enter a password';
-                          }
-                          return null;
+                          return _verifyPassword(value);
                         },
                       ),
                     ),
@@ -57,7 +56,7 @@ class SignupPage extends StatelessWidget {
                       child: ElevatedButton(
                         onPressed: () {
                           if (_formKey.currentState!.validate()) {
-                            // Process data.
+                            _addUser(context);
                           }
                         },
                         child: const Text('Submit'),
@@ -71,5 +70,47 @@ class SignupPage extends StatelessWidget {
         ),
       ),
     );
+  }
+
+  _verifyEmail(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter an email';
+    }
+    return null;
+  }
+
+  _verifyPassword(value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter a password';
+    }
+    return null;
+  }
+
+  _addUser(BuildContext context) async {
+    try {
+      final credential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: _inputEmail.text,
+        password: _inputPassword.text,
+      );
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text("Account has been created!"),
+      ));
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'weak-password') {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Password is too weak, please enter at least 6 characters."),
+        ));
+      } else if (e.code == 'email-already-in-use') {
+        ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+          content: Text("Email already exists, please try logging in using this email."),
+        ));
+      }
+      return;
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+        content: Text("There was an unknown error creating your account. Please try again later."),
+      ));
+      return;
+    };
   }
 }
