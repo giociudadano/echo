3bfd75ab311346052d9fae771529e00b55a541d9
Gio Ciudadano <gaciudadano@up.edu.ph>
2023-05-25 01:54:40 +0800

    Updates OnBoarding placeholder text

--- lib/content_model.dart
+++ lib/content_model.dart
@@ -12,19 +12,19 @@ final List<PageContent> contents = [
   PageContent(
     image: "lib/assets/images/onboarding/louder.png",
     title: "Louder",
-    description1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    description2: "Excepteur sint occaecat cupidatat non proident.",
+    description1: "Create a card and share it to different classes at the same time",
+    description2: "Join a class and see cards created by other users",
   ),
   PageContent(
     image: "lib/assets/images/onboarding/faster.png",
     title: "Faster",
-    description1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    description2: "Excepteur sint occaecat cupidatat non proident.",
+    description1: "Users are updated and notified in real-time",
+    description2: "Mark posts as done and increase user productivity",
   ),
   PageContent(
     image: "lib/assets/images/onboarding/clearer.png",
     title: "Clearer",
-    description1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    description2: "Excepteur sint occaecat cupidatat non proident.",
+    description1: "Search a card or filter cards by class",
+    description2: "Edit or delete cards and classes if you made a mistake",
   )
 ];
