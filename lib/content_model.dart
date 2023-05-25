cc0bc253b71405aea2d9988c102f1afef75269bd
Lloyd <lmdelacruz4@up.edu.ph>
2023-05-25 09:17:51 +0800

    updated onboarding messages

--- lib/content_model.dart
+++ lib/content_model.dart
@@ -12,19 +12,19 @@ final List<PageContent> contents = [
   PageContent(
     image: "lib/assets/images/onboarding/louder.png",
     title: "Louder",
-    description1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    description2: "Excepteur sint occaecat cupidatat non proident.",
+    description1: "all class announcements in one app",
+    description2: "contents are easy to spot and read",
   ),
   PageContent(
     image: "lib/assets/images/onboarding/faster.png",
     title: "Faster",
-    description1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    description2: "Excepteur sint occaecat cupidatat non proident.",
+    description1: "posting is done quickly through a form",
+    description2: "announcements may be sent to multiple groups at once",
   ),
   PageContent(
     image: "lib/assets/images/onboarding/clearer.png",
     title: "Clearer",
-    description1: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-    description2: "Excepteur sint occaecat cupidatat non proident.",
+    description1: "groups strictly contain announcements",
+    description2: "information are organized and presented in chunks",
   )
 ];
